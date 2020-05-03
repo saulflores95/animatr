274769c49912b365aa314776321bdb783eddfa3d
Saul Flores <saulpolo95@gmail.com>
2020-05-02 17:55:45 -0700

    Moved drag-n-drop logic, into figures.js, added utils page for share methods, add ace editors and beutify libs for code exporting, added export clear and load buttons/ functioanlity, json is used for loading and storing localStorage data

--- public/assets/scripts/exporter.js
+++ public/assets/scripts/exporter.js
@@ -1,3 +1,21 @@
+import { dragElement } from './utils.js';
+let oldText = ''
+/**
+ * we’ll first check if DOMParser exists in the window. 
+ * If not, we’ll return false. Next, we’ll try to use parseFromString() to create a document. 
+ * If it fails, we’ll return false. Otherwise, we’ll return true.
+ */
+let support = (function () {
+	if (!window.DOMParser) return false;
+	var parser = new DOMParser();
+	try {
+		parser.parseFromString('x', 'text/html');
+	} catch(err) {
+		return false;
+	}
+	return true;
+})();
+
 /**
  * Returns the text from a HTML string
  * 
@@ -17,19 +35,30 @@ let stripHtml = (html) => {
  * 
  * @param {} String The html string
  */
-let getBoardText = () => {
-    var board = document.getElementById("board");
-    var boardText = boardText.innerHTML;
-    console.log("boardText: ", boardText);
+window.getBoardText = () => {
+    let board = document.getElementById("board");
+    let boardText = board.innerHTML;
+    oldText = boardText
     return boardText;
 }
 
+window.loadBoard = (htmlString) => {
+    let board = document.getElementById("board");
+    console.log("oldText: ", oldText);
+    let childElement = stringToHTML(htmlString)
+    let array = [ ...childElement.childNodes ];
+    array.forEach(element => {
+        dragElement(element)
+        board.appendChild(element);
+    });
+
+}
 /**
  * Convert a template string into HTML DOM nodes
  * @param  {String} str The template string
  * @return {Node}       The template HTML
  */
-let stringToHTML = function (str) {
+let stringToHTML =  (str) => {
 
 	// If DOMParser is supported, use it
 	if (support) {
@@ -45,8 +74,60 @@ let stringToHTML = function (str) {
 
 };
 
-let addElementToBoard = function (children) { 
-    var board = document.getElementById("board");
-    element.appendChild(children);       
+/**
+ * appends a DOM element and appends it the drawing board/
+ * @param  {DOM} children The actual board element
+ * @return {}
+ */
+window.addElementToBoard = () => { 
+    let text = ""
+    let board = document.getElementById("board");
+    board.appendChild(children);       
 } ;
 
+window.clearBoard = () => {
+    let board = document.getElementById("board");
+    board.innerHTML = ""
+}
+
+window.getCSS = () => {
+    var css = [];
+    for (var i=0; i<document.styleSheets.length; i++)
+    {
+        var sheet = document.styleSheets[i];
+        var rules = ('cssRules' in sheet)? sheet.cssRules : sheet.rules;
+        if (rules)
+        {
+            css.push('\n/* Stylesheet : '+(sheet.href||'[inline styles]')+' */');
+            for (var j=0; j<rules.length; j++)
+            {
+                var rule = rules[j];
+                if ('cssText' in rule)
+                    css.push(rule.cssText);
+                else
+                    css.push(rule.selectorText+' {\n'+rule.style.cssText+'\n}\n');
+            }
+        }
+    }
+    var cssInline = css.join('\n')+'\n';
+}
+
+window.exportBoardToJson = () => {
+    let data = {
+        'html': getBoardText(),
+        'css': '',
+        'js':''
+    }
+    console.log('exporting: ', data);
+    return data;
+}
+
+let loadPreviouseSession = () => {
+    if(localStorage.animatrSession) {
+        clearBoard();
+        let animatorSession = JSON.parse(localStorage.getItem("animatrSession"));
+        console.log('loading previouse animatr session: ', localStorage.animatrSession);
+        loadBoard(animatorSession.html);
+    } 
+}
+loadPreviouseSession()
--- public/assets/scripts/figures.js
+++ public/assets/scripts/figures.js
@@ -1,64 +1,157 @@
+import { dragElement } from './utils.js';
+
+var board = document.getElementById("board");
 var triangle_id_num = 0;
 var circle_id_num = 0;
 var square_id_num = 0;
 var octagon_id_num = 0;
 
-function MakeTriangle()
-{
+var Triangle = document.getElementById("TriangleItem");
+var Circle = document.getElementById("CircleItem");
+var Square = document.getElementById("SquareItem");
+var Octagon = document.getElementById("OctagonItem");
+
+var i = 0;
+var flag = false;
+
+function dragStart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+Triangle.ondragstart = dragStart;
+Circle.ondragstart = dragStart;
+Square.ondragstart = dragStart;
+Octagon.ondragstart = dragStart;
+
+board.ondrop = function(e) {
+    e.preventDefault();
+    var div = null;
+    switch(e.dataTransfer.getData("text")) {
+        case "TriangleItem":
+            spawnTriangle(triangle_id_num);
+            triangle_id_num++
+            break;
+        case "CircleItem":
+            spawnCircle(circle_id_num);
+            circle_id_num++;
+            break;
+        case "SquareItem":
+            spawnSquare(square_id_num);
+            square_id_num++;
+            break;
+        case "OctagonItem":
+            spawnOctagon(octagon_id_num);
+            octagon_id_num++;
+            break
+        default:
+            break;
+    }
+
+    function spawnDiv(type, num) {
+        div.style.position = "absolute";
+        div.style.top = (e.clientY - board.offsetTop - 25) + "px";
+        div.style.left = (e.clientX - board.offsetLeft - 25) + "px";
+        div.id = type + num;
+        i++;
+        board.appendChild(div);
+        dragElement(document.getElementById(div.id));
+    }
+
+    function spawnTriangle(idnum) {
+        div = document.createElement('div');
+        div.style.height = "0";
+        div.style.width = "0";
+        div.style.borderLeft= "25px solid transparent";
+        div.style.borderRight= "25px solid transparent";
+        div.style.borderBottom= "50px solid green";
+        spawnDiv("triangle_", idnum);
+    }
+
+    function spawnCircle(idnum) {
+        div = document.createElement('div');
+        div.style.height= "50px";
+        div.style.width= "50px";
+        div.style.background= "red";
+        div.style.borderRadius= "50%";
+        div.style.align= "center";
+
+        spawnDiv("circle_", idnum);
+    }
+
+    function spawnSquare(idnum) {
+        div = document.createElement('div');
+        div.style.height= "50px";
+        div.style.width= "50px";
+        div.style.background= "blue";
+        div.style.align= "center";
+
+        spawnDiv("square_", idnum);
+    }
+
+    function spawnOctagon(idnum) {
+        div = document.createElement('div');
+        div.className = "octagon";
+        
+        spawnDiv("octagon_", idnum);
+    }
+}
+
+board.ondragover = function(e) {
+    e.preventDefault();
+}
+
+window.MakeTriangle = () => {
     var div = document.createElement('div');    
     div.style.height = "0";
     div.style.width = "0";
     div.style.borderLeft= "25px solid transparent";
     div.style.borderRight= "25px solid transparent";
     div.style.borderBottom= "50px solid green";
+    div.style.position = "absolute";
 
     div.id = "triangle_" + triangle_id_num;
     triangle_id_num++;
-    console.log(div.id);
-
+    dragElement(div);
     document.getElementById("board").appendChild(div); 
 
 }
 
-function MakeCircle()
-{
+window.MakeCircle = () => {
     var div = document.createElement('div');
     div.style.height= "50px";
     div.style.width= "50px";
     div.style.background= "red";
     div.style.borderRadius= "50%";
     div.style.align= "center";
+    div.style.position = "absolute";
 
     div.id = "circle_" + circle_id_num;
     circle_id_num++;
-    console.log(div.id);
-    
+    dragElement(div);
     document.getElementById("board").appendChild(div); 
 }
 
-function MakeSquare()
-{
+window.MakeSquare = () => {
     var div = document.createElement('div');
     div.style.height= "50px";
     div.style.width= "50px";
     div.style.background= "blue";
     div.style.align= "center";
-    
+    div.style.position = "absolute";
+
     div.id = "square_" + square_id_num;
     square_id_num++;
-    console.log(div.id);
-    
+    dragElement(div);
     document.getElementById("board").appendChild(div); 
 }
 
-function MakeOctagon()
-{
+window.MakeOctagon = () =>{
     var div = document.createElement('div');
     div.className = "octagon";
-
+    div.style.position = "absolute";
     div.id = "octagon_" + octagon_id_num;
     octagon_id_num++;
-    console.log(div.id);
-    
+    dragElement(div);
     document.getElementById("board").appendChild(div); 
 }
+
